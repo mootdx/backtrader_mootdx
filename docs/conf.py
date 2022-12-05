951521a3fd127a9a6cad73df3571b2b80fedc487
bopo <ibopo@126.com>
2022-12-05 09:25:31 +0800

    update commit

--- docs/conf.py
+++ docs/conf.py
@@ -64,7 +64,7 @@ release = backtrader_mootdx.__version__
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = 'zh_CN'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -83,8 +83,8 @@ todo_include_todos = False
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = 'alabaster'
-# html_theme = "furo"
+# html_theme = 'alabaster'
+html_theme = "furo"
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
--- docs/index.rst
+++ docs/index.rst
@@ -1,5 +1,5 @@
 Welcome to backtrader_mootdx's documentation!
-======================================
+===============================================
 
 .. toctree::
    :maxdepth: 2
